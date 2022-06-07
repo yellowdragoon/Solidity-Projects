bd973e2c2f0e3a16fc3814c3ade1d13bc5f355c7
yellowdragoon <lihnry0813@gmail.com>
2022-06-07 12:00:30 +0100

    simple counter

---
+++ Owner.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8;
+
+contract Owner{
+
+    address public owner;
+    uint public counter;
+
+    constructor(uint _count){
+        owner = msg.sender;
+        counter = _count;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
+    }
+
+    function incrementCounter() external {
+        counter++;
+    }
+
+    function decrementCounter() external {
+        counter--;
+    }
+
+    function setCount(uint _count) external onlyOwner{
+        counter = _count;
+    }
+
+    function transferOwner(address _newOwner) external onlyOwner{
+        require(_newOwner != address(0));
+        owner = _newOwner;
+    }
+
+
+}
